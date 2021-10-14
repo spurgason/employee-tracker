725639a49a7f3bbe492993a483fcb41a750cd568
spurgason <shanepurgason.98@gmail.com>
2021-10-14 15:03:06 -0400

    added initial prompt

---
+++ index.js
@@ -0,0 +1,46 @@
+const inquirer = require('inquirer');
+
+
+const promptUser = () => {
+    
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'selectAction',
+                message: "What would you like to do?",
+                choices: [
+                    'View all departments',
+                    'View all roles',
+                    'View all employees',
+                    'Add a department',
+                    'Add a role',
+                    'Add an employee',
+                    'Update an employee role'
+                ]
+            }
+        ]).then(({selectAction}) => {
+            if (selectAction === 'View all departments') {
+                
+            } 
+            if (selectAction === 'View all roles') {
+                
+            } 
+            if (selectAction === 'View all employees') {
+                
+            } 
+            if (selectAction === 'Add a department') {
+                
+            } 
+            if (selectAction === 'Add a role') {
+                
+            } 
+            if (selectAction === 'Add an employee') {
+                
+            } 
+            if (selectAction === 'Update an employee role') {
+                
+            } 
+        })
+
+}
